851af0ebada09bf5fb1d4166a7bb2a3536881999
Austine Mwanyalo <mwanyaloa@outlook.com>
2019-09-16 17:04:27 +0300

    Integrated Auth0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './Home';
+import Nav from './Nav';
+import Profile from './Profile';
+import Auth from './Auth/Auth';
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
+
+  render() {
+    return (
+      <>
+        <Nav />
+        <div className='body'>
+          <Route
+            path='/'
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route path='/profile' component={Profile} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
